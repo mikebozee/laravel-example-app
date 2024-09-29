081acd5ae1433d2978cb7bd590a16116fedbe7bc
mikebozee <mike@mikebozee.com>
2024-09-29 13:58:03 -0400

    Complete lesson 24.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
